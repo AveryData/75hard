7acc0861e518f06a8dded29ca167b813e8f45bc6
AveryData <averysmith@MacBook-Pro-46.local>
2022-05-08 12:11:31 -0600

    Create 75Hard.py
    added the demo py

---
+++ 75Hard.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  8 12:03:59 2022
+
+@author: averysmith
+"""
+
+import streamlit as st
+import pandas as pd
+
+st.title('Ken + Averys NFT Wallet Analyzer')
+st.write('NFTs are awesome, but there is a big problem in the comment and that is people sending unsolicited NFTs to celebrities wallets and claiming they support the contract. Currently, it is hard to see if the wallet owner actually purchased the NFT, or was just gifted it. This app aims to address that problem')
